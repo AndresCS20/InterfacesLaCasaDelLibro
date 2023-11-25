8a5b8ba47c0468cd6f04dbad55bee88d11f2bc8c
AndresCS20 <andres.calleja-sanchez@iessampedro.org>
2023-11-25 17:06:45 +0100

    Actualizado el ISBN y la portada de los Libros .

--- src/laCasaDelLibro/Main.java
+++ src/laCasaDelLibro/Main.java
@@ -10,16 +10,16 @@ public class Main {
 	
 	public static void main(String[] args) {
 		
-		Libro libro1 = new Libro("978-0345803481", "Ready Player One", "Ernest Cline", 20, "http://example.com/readyplayerone.jpg");
-        Libro libro2 = new Libro("978-0439064873", "Harry Potter and the Chamber of Secrets", "J.K. Rowling", 30, "http://example.com/harrypotter.jpg");
-        Libro libro3 = new Libro("978-0451524935", "1984", "George Orwell", 25, "http://example.com/1984.jpg");
-        Libro libro4 = new Libro("978-0140283334", "Fight Club", "Chuck Palahniuk", 15, "http://example.com/fightclub.jpg");
-        Libro libro5 = new Libro("978-0307474278", "The Girl with the Dragon Tattoo", "Stieg Larsson", 10, "http://example.com/dragontattoo.jpg");
-        Libro libro6 = new Libro("978-0062457790", "The Alchemist", "Paulo Coelho", 35, "http://example.com/alchemist.jpg");
-        Libro libro7 = new Libro("978-0307887443", "The Lean Startup", "Eric Ries", 40, "http://example.com/leanstartup.jpg");
-        Libro libro8 = new Libro("978-1591847786", "Zero to One", "Peter Thiel", 30, "http://example.com/zerotoone.jpg");
-        Libro libro9 = new Libro("978-0062316110", "Thinking Fast and Slow", "Daniel Kahneman", 20, "http://example.com/thinkingfastandslow.jpg");
-        Libro libro10 = new Libro("978-0062225672", "Divergent", "Veronica Roth", 25, "http://example.com/divergent.jpg");
+		Libro libro1 = new Libro("9780345803481", "Ready Player One", "Ernest Cline", 20, "https://imagessl9.casadellibro.com/a/l/s7/79/9788466649179.webp");
+        Libro libro2 = new Libro("9780439064873", "Harry Potter and the Chamber of Secrets", "J.K. Rowling", 30, "https://imagessl9.casadellibro.com/a/l/s7/69/9781408855669.webp");
+        Libro libro3 = new Libro("9780451524935", "1984", "George Orwell", 25, "https://imagessl4.casadellibro.com/a/l/s7/44/9788499890944.webp");
+        Libro libro4 = new Libro("9780140283334", "Fight Club", "Chuck Palahniuk", 15, "https://imagessl5.casadellibro.com/a/l/s7/55/9781448114955.webp");
+        Libro libro5 = new Libro("9780307474278", "The Girl with the Dragon Tattoo", "Stieg Larsson", 10, "https://imagessl6.casadellibro.com/a/l/s7/36/9780857054036.webp");
+        Libro libro6 = new Libro("9780062457790", "The Alchemist", "Paulo Coelho", 35, "https://imagessl8.casadellibro.com/a/l/s7/68/9783966619868.webp");
+        Libro libro7 = new Libro("9780307887443", "The Lean Startup", "Eric Ries", 40, "https://imagessl1.casadellibro.com/a/l/s7/81/9783963134081.webp");
+        Libro libro8 = new Libro("9781591847786", "Zero to One", "Peter Thiel", 30, "https://imagessl4.casadellibro.com/a/l/s7/04/9780753550304.webp");
+        Libro libro9 = new Libro("9780062316110", "Thinking Fast and Slow", "Daniel Kahneman", 20, "https://imagessl0.casadellibro.com/a/l/s7/70/9780141033570.webp");
+        Libro libro10 = new Libro("9780062225672", "Divergent", "Veronica Roth", 25, "https://imagessl9.casadellibro.com/a/l/s7/79/9788413141879.webp");
         	
 		listaLibros.addAll(Arrays.asList(libro1,libro2,libro3,libro4,libro5,libro6,libro7,libro8,libro9,libro10));
 		
