5f9e9844d530c03659e6675b48372519df52e1c9
Pablo6312 <pablodiaz6312@gmail.com>
2023-12-03 01:03:46 +0100

    Commit final VerMasLibro y ModificarLibro (revisar)
    Revisar como refrescar la ventana VerMasLibro cuando se modificar un libro, y lo del modal que esta puesto y no funciona.

--- src/laCasaDelLibro/ModificarLibro.java
+++ src/laCasaDelLibro/ModificarLibro.java
@@ -18,10 +18,23 @@ import java.awt.Font;
 import java.awt.Image;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
 
 import javax.swing.SwingConstants;
 
 public class ModificarLibro extends JDialog {
+	
+	public  ArrayList<String> listaErrores=new ArrayList<String>();
+	String error1= "El ISBN tiene que tener 13 caracteres. ";
+	String error2= "Introduce un título, está vacio. ";
+	String error3 = "Introduce un autor, está vacío. ";
+	String error4 = "Introduce una cantidad. ";
+	String error5 = "Cantidad debe ser un número entero. ";
+	String error6 = "Introduce el enlace de la imagen. ";
+	String error7 = "El ISBN no puede contener comas. ";
+	String error8 = "El título no puede contener comas. ";
+	String error9 = "El autor no puede contener comas. ";
+	String error10 = "El enlace no puede contener comas. ";
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
@@ -35,6 +48,8 @@ public class ModificarLibro extends JDialog {
 	private JLabel labelEnlaceImagen;
 	private JTextField textFieldEnlaceImagen;
 	private JLabel labelTituloModificar;
+	private Libro libro;
+//	private VerMasLibro verMas = new VerMasLibro();
 
 	/**
 	 * Launch the application.
@@ -55,9 +70,11 @@ public class ModificarLibro extends JDialog {
 	/**
 	 * Create the frame.
 	 */
-	public ModificarLibro() {
+	public ModificarLibro(Libro libroMod) {
+		setModal(true);
+		this.libro = libroMod;
 		setResizable(false);
-		setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE); //PUEDE QUE ESTE EL PROBLEMA ACÁ
+		setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);//PUEDE QUE ESTE EL PROBLEMA ACÁ
 		setBounds(100, 100, 691, 481);
 		contentPane = new JPanel();
 		contentPane.setBackground(new Color(255, 255, 255));
@@ -69,8 +86,6 @@ public class ModificarLibro extends JDialog {
 		logo();
 		
 		isbn();
-		
-
 		
 		titulo();
 		
@@ -109,21 +124,17 @@ public class ModificarLibro extends JDialog {
 	}
 
 	private void imagenLibro() throws MalformedURLException {
-		URL enlaceImagen = new URL("https://imagessl9.casadellibro.com/a/l/s7/79/9788466649179.webp");
-		
-		
-		JPanel panelImagenLibro = new JPanel();
-		panelImagenLibro.setBounds(386, 104, 253, 298);
-		contentPane.add(panelImagenLibro);
+		URL enlaceImagen = new URL(libro.getUrlImagen());
         ImageIcon icon = new ImageIcon(enlaceImagen);
-        Image image = icon.getImage().getScaledInstance(contentPane.WIDTH,contentPane.HEIGHT, Image.SCALE_SMOOTH); // Ajusta el tamaño a 100x100
-        icon = new ImageIcon(image);
+        Image image = icon.getImage();
         
-		ImageIcon imageIllustration = new ImageIcon(enlaceImagen);
-//		ImageIcon imageIllustration = new ImageIcon("./images/Bookshop-ill.png");
-
-		JLabel labelImagenLibro = new JLabel(imageIllustration);
-		panelImagenLibro.add(labelImagenLibro);
+        JLabel imagenPortada = new JLabel();
+        imagenPortada.setBounds(386, 104, 253, 298);
+        
+        image = image.getScaledInstance(imagenPortada.getWidth(), imagenPortada.getHeight(), Image.SCALE_DEFAULT);
+        
+		imagenPortada.setIcon(new ImageIcon(image));
+		contentPane.add(imagenPortada);
 	}
 
 	private void isbn() {
@@ -132,8 +143,10 @@ public class ModificarLibro extends JDialog {
 		contentPane.add(labelISBN);	
 		
 		textFieldISBN = new JTextField();
+		textFieldISBN.setEditable(false);
 		textFieldISBN.setBounds(39, 120, 238, 27);
 		contentPane.add(textFieldISBN);
+		textFieldISBN.setText(this.libro.getISBN());
 		textFieldISBN.setColumns(10);
 	}
 
@@ -146,6 +159,7 @@ public class ModificarLibro extends JDialog {
 		textFieldTitulo.setToolTipText("");
 		textFieldTitulo.setColumns(10);
 		textFieldTitulo.setBounds(39, 174, 238, 27);
+		textFieldTitulo.setText(this.libro.getTitulo());
 		contentPane.add(textFieldTitulo);
 	}
 
@@ -158,6 +172,7 @@ public class ModificarLibro extends JDialog {
 		textFieldNombreAutor.setToolTipText("");
 		textFieldNombreAutor.setColumns(10);
 		textFieldNombreAutor.setBounds(39, 228, 238, 27);
+		textFieldNombreAutor.setText(this.libro.getAutor());
 		contentPane.add(textFieldNombreAutor);
 	}
 
@@ -170,6 +185,7 @@ public class ModificarLibro extends JDialog {
 		textFieldCantidad.setToolTipText("");
 		textFieldCantidad.setColumns(10);
 		textFieldCantidad.setBounds(39, 283, 238, 27);
+		textFieldCantidad.setText(String.valueOf(this.libro.getCantidad()));
 		contentPane.add(textFieldCantidad);
 	}
 
@@ -182,10 +198,12 @@ public class ModificarLibro extends JDialog {
 		textFieldEnlaceImagen.setToolTipText("");
 		textFieldEnlaceImagen.setColumns(10);
 		textFieldEnlaceImagen.setBounds(39, 336, 238, 27);
+		textFieldEnlaceImagen.setText(this.libro.getUrlImagen());
 		contentPane.add(textFieldEnlaceImagen);
 	}
 
 	private void botonCrear() {
+		
 		JButton btnCrearLibro = new JButton("Modificar Libro");
 		btnCrearLibro.setForeground(new Color(255, 255, 255));
 		btnCrearLibro.setFont(new Font("Segoe UI", Font.BOLD, 14));
@@ -193,6 +211,82 @@ public class ModificarLibro extends JDialog {
 		btnCrearLibro.setBounds(39, 374, 238, 27);
 		contentPane.add(btnCrearLibro);
 		
+		btnCrearLibro.addActionListener(e->{
+			listaErrores.clear();
+
+			String isbn = textFieldISBN.getText();
+			comprobarISBN(isbn);
+			String titulo = textFieldTitulo.getText();
+			comprobarTitulo(titulo);
+			String nombre = textFieldNombreAutor.getText();
+			comprobarAutor(nombre);
+			int cantidad = 0;
+			try {
+			 cantidad = Integer.parseInt(textFieldCantidad.getText());
+			} catch (NumberFormatException e1) {
+			listaErrores.add(error5);
+			}
+			String url = textFieldEnlaceImagen.getText();
+			comprobarEnlace(url);
+			String totalErrores="";
+			if(listaErrores.size()>0) {
+				for(String error : listaErrores) {
+				 totalErrores+=error;
+				}
+				JOptionPane.showMessageDialog(null, totalErrores, "Ha ocurrido un error", JOptionPane.WARNING_MESSAGE); //Crear alerta al crear el objeto correctamente (tambien lo puedes usar para debugear
+
+				System.out.println(totalErrores);
+			}
+			
+			this.libro.setTitulo(titulo);
+			this.libro.setAutor(nombre);
+			this.libro.setCantidad(cantidad);
+			this.libro.setUrlImagen(url);
+			
+			System.out.println(Main.listaLibros.toString());
+			System.out.println(Main.listaLibros.size());
+			
+			
+		});
+		
 		//JOptionPane.showMessageDialog(null, "Este es un mensaje de alerta", "Correcto", JOptionPane.DEFAULT_OPTION); Crear alerta al crear el objeto correctamente (tambien lo puedes usar para debugear
 	}
+	
+	public void comprobarISBN(String isbn) {
+		if(isbn.length()!=13) {
+			listaErrores.add(error1);
+		}
+		if(isbn.contains(",")) {
+			listaErrores.add(error7);
+		}
+	}
+	
+	public void comprobarTitulo(String titulo) {
+		if(titulo.length()==0) {
+			listaErrores.add(error2);
+		}
+		if(titulo.contains(",")) {
+			listaErrores.add(error8);
+		}
+	}
+	
+	
+	public void comprobarAutor(String autor) {
+		if (autor.length()==0){
+			listaErrores.add(error3);
+			
+		}if(autor.contains(",")) {
+			listaErrores.add(error9);
+		}
+	}
+	
+	public void comprobarEnlace(String enlace) {
+		if(enlace.length()==0) {
+			listaErrores.add(error6);
+		}
+		if(enlace.contains(",")) {
+			listaErrores.add(error10);
+		}
+	}
+	
 }
